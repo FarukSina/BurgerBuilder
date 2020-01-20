dcfb71d66bb3cde726dec13cc9d0a3285f124396
FarukSina <200437201@student.georgianc.on.ca>
2020-01-19 21:08:41 -0500

    Updates

---
+++ precache-manifest.3ec33baff23b159af6d262604b1f3ab2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4fe505bb98ca35f3a54129b1aa0f0eeb",
+    "url": "/FarukSina/BurgerBuilder.git/index.html"
+  },
+  {
+    "revision": "10f42ea9357c7e3e4eee",
+    "url": "/FarukSina/BurgerBuilder.git/static/css/main.dc6e0f78.chunk.css"
+  },
+  {
+    "revision": "6ddd8bd9cb29c2ad077f",
+    "url": "/FarukSina/BurgerBuilder.git/static/js/2.e7bd2b9a.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/FarukSina/BurgerBuilder.git/static/js/2.e7bd2b9a.chunk.js.LICENSE"
+  },
+  {
+    "revision": "10f42ea9357c7e3e4eee",
+    "url": "/FarukSina/BurgerBuilder.git/static/js/main.a71d9110.chunk.js"
+  },
+  {
+    "revision": "a459cce96236703f4221",
+    "url": "/FarukSina/BurgerBuilder.git/static/js/runtime-main.e88bcab8.js"
+  },
+  {
+    "revision": "b8503d262bffbfb7c67fd6762963e7d1",
+    "url": "/FarukSina/BurgerBuilder.git/static/media/burger-logo.b8503d26.png"
+  }
+]);
